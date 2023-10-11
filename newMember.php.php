27b37c8d47291190c74882368ab14f79ee08ab73
Ben Flood <B00145093@mytudublin.ie>
2023-10-11 10:42:13 +0100

    finished part 2

---
+++ newMember.php.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form method="POST" action="Part2.php">
+            <p>Enter your name</p>
+            <input name="firstName"/>
+            <br>
+            <input name="surName"/>
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
